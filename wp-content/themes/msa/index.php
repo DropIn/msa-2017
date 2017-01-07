6e7c525b954e5548481ca34a8fc12d48ddc7bbe3
Björn Zschernack <bz@dergoldbroiler.de>
2017-01-07 14:08:26 +0100

    changed template to have a clean registry form

--- wp-content/themes/msa/index.php
+++ wp-content/themes/msa/index.php
@@ -16,17 +16,6 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
-
 			<?php
 			// Start the loop.
 			while ( have_posts() ) : the_post();
@@ -36,17 +25,12 @@ get_header(); ?>
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'content', get_post_format() );
+				get_template_part( 'content' );
 
 			// End the loop.
 			endwhile;
 
-			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-				'next_text'          => __( 'Next page', 'twentyfifteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
-			) );
+			
 
 		// If no content, include the "No posts found" template.
 		else :
@@ -55,7 +39,5 @@ get_header(); ?>
 		endif;
 		?>
 
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
 
 <?php get_footer(); ?>
